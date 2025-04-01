9cc078be67ff42bcbf5bb74152c4aec7034de4eb
NouraSharif <sharifnoura413@gmail.com>
2025-04-01 19:14:07 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/app22/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app22
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:app22/firebase_options.dart';
+import 'package:app22/login.dart';
+import 'package:app22/register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    //هاي دالة streem اللي بتسمع على حالة المستخدم
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print(
+          '==================================================User is currently signed out!',
+        );
+      } else {
+        print(
+          '==================================================User is signed in!',
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const login100(),
+      routes: {
+        "register": (context) => const Register20(),
+        "login": (context) => const login100(),
+      },
+    );
+  }
+}
