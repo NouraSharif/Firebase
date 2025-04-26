7fec3ad6d8ac2871cac20464f4a25f224df20e93
NouraSharif <sharifnoura413@gmail.com>
2025-04-26 16:37:07 +0300

    Cloud Messaging permissions IOS **Web

--- lib/cloudMessaging.dart
+++ lib/cloudMessaging.dart
@@ -9,6 +9,8 @@ class TestState extends StatefulWidget {
 }
 
 class _TestStateState extends State<TestState> {
+  //لاني بشتغل على محاكي كروم ما بيقبل يطبعلي التوكن لازم اشتغل على محاكي اندرويد
+  //او جهاز حقيقي
   getToken() async {
     String? tokenname = await FirebaseMessaging.instance.getToken();
     print(
@@ -16,8 +18,36 @@ class _TestStateState extends State<TestState> {
     );
   }
 
+  /*
+  في الويب ➔ المتصفح مسؤول عن طلب الإذن.
+
+  في الموبايل ➔ إنت (كمطور) لازم تطلب الإذن من الكود.
+  */
+  //في الويب بلاتفورم والايفون وبعض الاصدارت الجديدة من الاندرويد التطبيق ماعندهم الاذن الخاص بالوصول للاشعارات
+  //بهاي الحالة رح نستخدمpermission
+
+  myRequestPermission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    NotificationSettings settings = await messaging.requestPermission(
+      //الخصائص هاي خاصة بالابل حسب ال
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
   @override
   void initState() {
+    //  myRequestPermission();
     getToken();
     super.initState();
   }
